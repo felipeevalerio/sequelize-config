f6bee45e7ce3064821070bf49a287bb2e4927eff
felipe-spindola <felipeevalerio@outlook.com>
2021-06-21 15:27:39 -0300

    relacionamento N-N

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = express.Router();
 const UserController = require("./controllers/UserController");
 const AddressController = require("./controllers/AddressController");
+const TechController = require("./controllers/TechController");
+
 
 
 routes.get("/",(req,res) => {
@@ -14,6 +16,10 @@ routes.post("/users",UserController.store);
 
 routes.get("/users/:user_id/addresses",AddressController.index);
 routes.post("/users/:user_id/addresses",AddressController.store);
+
+routes.get("/users/:user_id/techs",TechController.index);
+routes.post("/users/:user_id/techs",TechController.store);
+routes.delete("/users/:user_id/techs",TechController.delete);
 
 
 module.exports = routes;
